97f6bae873e1129b54eee0fb2087ba54f0bd4613
Towker Ahmmed Joy <72230924+Towkerjoy@users.noreply.github.com>
2021-06-26 12:52:03 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Navigation Menu
-const menuToggle = document.querySelector(".toggle");
-const menu = document.querySelector(".navigation");
-const secHero = document.querySelector(".sec");
-menuToggle.onclick = function(){
-    menuToggle.classList.toggle("active");
-    menu.classList.toggle("active");
-    secHero.classList.toggle("active");
-}
